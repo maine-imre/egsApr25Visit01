f7bc234de846d4bf4319601bb324baa11c6104c9
Camden Bock <camden.bock@maine.edu>
2019-04-25 09:40:45 -0400

    added dude and floor.
    removed crotching attached sword

---
+++ Assets/Scripts/attack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+    public GameObject sword;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //attack
+            
+            //swing
+            //hitthings
+            
+        } 
+    }
+}
